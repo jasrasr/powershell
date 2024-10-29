c27d51091759c751e369fd9284e8a5b9fef4c74e
jasrasr <92162022+jasrasr@users.noreply.github.com>
2024-10-29 12:01:04 -0400

    Auto-sync commit 2024-10-29 12:01:04

---
+++ Regedit-Scripts/detect reg for autodesk.ps1
@@ -0,0 +1,2 @@
+﻿$autodeskPrograms = Get-ChildItem -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall, HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall | Get-ItemProperty | Where-Object {($_.DisplayName -like "*Autodesk*") -or ($_.Publisher -like "*Autodesk*")} | Sort-Object DisplayName
+$autodeskPrograms | FT -autosize
