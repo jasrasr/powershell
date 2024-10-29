5ebd5ab723f851d1768c8cad90c039c9a601d3ce
jasrasr <92162022+jasrasr@users.noreply.github.com>
2024-10-29 00:01:02 -0400

    Auto-sync commit 2024-10-29 00:01:02

---
+++ File-Management-Scripts/get overall folder size with child items in mb.ps1
@@ -0,0 +1,21 @@
+﻿# Define an array of project folders
+$projectfolders = @(
+    "N:\Citgo\CID 2024\CID2403",
+    "N:\Citgo\CID 2024\CID2404",
+    "N:\Citgo\CID 2024\CID2405",
+    "N:\Citgo\CID 2024\CID2406",
+    "N:\Citgo\CID 2024\CID2407"
+    )
+
+    
+# Loop through each folder and calculate the total size
+foreach ($projectfolder in $projectfolders) {
+    if (Test-Path $projectfolder) {
+        $folderSize = (Get-ChildItem -Path $projectfolder -Recurse | Measure-Object -Property Length -Sum).Sum
+        $folderSizeMB = [math]::Round($folderSize / 1MB, 2)
+        Write-Output "Total size of $projectfolder : $folderSizeMB MB"
+    }
+    else {
+        Write-Output "Folder $projectfolder does not exist."
+    }
+}
