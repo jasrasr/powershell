eeeb445c2fcb8492073e6048aa4bb5fd15a1ccae
jasrasr <92162022+jasrasr@users.noreply.github.com>
2025-02-17 01:08:10 -0500

    git commit PowerShell function

--- AD-Scripts/check-azure-for-mfa-issues.ps1
+++
@@ -1,5 +0,0 @@
-Install-Module MsIdentityTools -Scope CurrentUser
-
-Connect-MgGraph -Scopes Directory.Read.All, AuditLog.Read.All, UserAuthenticationMethod.Read.All
-
-Export-MsIdAzureMfaReport .\report.xlsx
--- AD-Scripts/get weak passwords.ps1
+++
@@ -1,6 +0,0 @@
-install-module dsinternals
-import-module dsinternals
-$weakpasswordsfilepath="c:\temp\passwords.txt"//-
-$server="cledc1"
-get-adreplaccount -samaccount jason.lamb -server $server
- -weakpasswordsfilepath $weakpasswordsfilepath -fullreport -verbos
