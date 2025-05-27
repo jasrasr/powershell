6f082ccd31e94a191d2374a295a9465441c12802
jasrasr <92162022+jasrasr@users.noreply.github.com>
2025-05-27 12:56:20 -0400

    Update adjust-shared-calendar-timezone.ps1
    make generic

--- ExchangeOnline-Scripts/adjust-shared-calendar-timezone.ps1
+++ ExchangeOnline-Scripts/adjust-shared-calendar-timezone.ps1
@@ -1,22 +1,12 @@
 # 1. Import the Exchange Online PowerShell module and connect
-#Import-Module ExchangeOnlineManagement
-#Connect-ExchangeOnline -UserPrincipalName '[email]'
+Import-Module ExchangeOnlineManagement
+Connect-ExchangeOnline -UserPrincipalName '[email]'
 
 # 2. Define the array of conference/mailbox email addresses
 $CalendarMailboxes = @(
-    "CLE-ConferenceA1@$domain.com",
-    "CLE-ConferenceA2@$domain.com",
-    "CLE-Conferenceb1@$domain.com",
-    "CLE-Conferenceb2@$domain.com",
-    "CLE-Conferencec1@$domain.com",
-    "CLE-Conferencec2@$domain.com",
-    "CLE-Conferenced1@$domain.com",
-    "CLE-Conferenced2@$domain.com",
-    "CLE-Conferencedevelopmentcenter@$domain.com",
-    "CLE-Conferenceboardroom@$domain.com",
-    "CLE-Conferencemain@$domain.com",
-    "CLE-insta360-camera@$domain.com"
-)
+   "calendar-email1@domain.com",
+    "calendar-email1@domain.com"
+   )
 
 # 3. Loop through each mailbox and set the desired properties
 foreach ($Mailbox in $CalendarMailboxes) {
@@ -34,7 +24,7 @@ foreach ($Mailbox in $CalendarMailboxes) {
         #-WorkingDays Monday,Tuesday,Wednesday,Thursday,Friday
 
     # (Optional) Update the Office/Location property
-    #Set-Mailbox -Identity $Mailbox -Office "Cleveland Headquarters"
+    #Set-Mailbox -Identity $Mailbox -Office "Office Headquarters"
 }
 
 # 4. Disconnect (optional)
