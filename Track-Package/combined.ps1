804345d2026fc82362371e4e19d34ad08eeceb7a
jasrasr <92162022+jasrasr@users.noreply.github.com>
2025-07-03 12:45:40 -0400

    Update combined.ps1
    updated with proper script header and regex

--- Track-Package/combined.ps1
+++ Track-Package/combined.ps1
@@ -1,10 +1,18 @@
+# Revision : 1.1
+# Description : Unified FedEx, UPS, USPS tracking function with updated regex detection. Rev 1.1
+# call with track-package ###
+# the script should recognize the format of the 3 carriers tracking numbers
+# Author : Jason Lamb
+# Created Date : 2025-07-03
+# Modified Date : 2025-07-03
+
 function Get-Carrier {
     param([string]$tn)
     switch -regex ($tn) {
-        '^(?i)1Z[0-9A-Z]{16}$'                { return 'UPS' }
-        '^(?i)\d{12}$'                        { return 'FedEx' }
-        '^(?i)9[0-9]{21}$'                    { return 'USPS' }
-        default                               { return $null }
+        '^(1Z[0-9A-Z]{16})$'                                     { return 'UPS' }
+        '^(?:\d{12}|\d{15}|\d{20}|\d{22})$'                       { return 'FedEx' }
+        '^(?:(94|93|92|95)[0-9]{20,22}|[A-Z]{2}[0-9]{9}[A-Z]{2})$' { return 'USPS' }
+        default                                                  { return $null }
     }
 }
 
@@ -13,7 +21,8 @@ function Track-Package {
 
     $carrier = Get-Carrier $TrackingNumber
     if (-not $carrier) {
-        Write-Warning "Unrecognized tracking number format: $TrackingNumber"; return
+        Write-Warning "Unrecognized tracking number format : $TrackingNumber"
+        return
     }
 
     switch ($carrier) {
@@ -22,8 +31,8 @@ function Track-Package {
         'USPS'  { $url = "https://tools.usps.com/go/TrackConfirmAction?qtc_tLabels1=$TrackingNumber" }
     }
 
-    Write-Output "Carrier detected: $carrier"
-    Write-Output "Tracking URL: $url"
+    Write-Host "Carrier detected : $carrier"
+    Write-Host "Tracking URL : $url"
 
     Start-Process chrome.exe "--new-window $url"
 }
