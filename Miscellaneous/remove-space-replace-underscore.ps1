f2f3945a9f7f3e4fd859be1674691938208835e1
Jason Lamb <92162022+jasrasr@users.noreply.github.com>
2026-01-09 15:32:01 -0500

    Modify script to accept user input and copy result
    Updated script to take user input and add output to clipboard.

--- Miscellaneous/remove-space-replace-underscore.ps1
+++ Miscellaneous/remove-space-replace-underscore.ps1
@@ -1,9 +1,19 @@
-$text1 = 'this is some test-text'
-# replace space with _
-$text2 = $text1 -replace ' ', '_'
-# replace - with _
-$text3 = $text2 -replace '-', '_'
-# replace multiple _ with single _
-$text4 = $text3 -replace '_+', '_'
-# output final result
-$text4
+    # ask for user input text
+    $text0 = read-host("enter some text:")
+
+    # $text1 = 'this is some test-text'
+
+    # replace space with _
+    $text2 = $text0 -replace ' ', '_'
+    
+    # replace - with _
+    $text3 = $text2 -replace '-', '_'
+    
+    # replace multiple _ with single _
+    $text4 = $text3 -replace '_+', '_'
+    
+    # output final result
+    $text4
+    
+    # add to clipboard
+    $text4 | Set-Clipboard
