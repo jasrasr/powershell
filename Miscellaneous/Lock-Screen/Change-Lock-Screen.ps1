d4b4f9d7aa60144cf3bb6ccf99e4efcab1a80c8c
Jason Lamb <92162022+jasrasr@users.noreply.github.com>
2026-03-12 13:57:35 -0400

    Update Change-Lock-Screen.ps1 with image saving instructions
    Add instructions for saving lock screen images on a server.

---
+++ Miscellaneous/Lock-Screen/Change-Lock-Screen.ps1
@@ -0,0 +1,6 @@
+<# 
+1. save files on a server somewhere accessible
+1.1 \\servername\images\
+1.2 files: Laptop_16-10.jpg, Portrait.jpg, Standard_16-9.jpg, SuperUltrawide_32-9.jpg, Ultrawide_21-9.jpg
+
+#>
